5887cf9948ce08de77d4c991fb40c8cc5a61ec2f
Jacob Shore <jshore@datalign.net>
2020-06-29 00:56:09 +0300

    many to many

--- app.py
+++ app.py
@@ -2,26 +2,42 @@ from os import getenv
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
-app = Flask(__name__)
 user = getenv('DB_USER_NAME')
 pwd = getenv('DB_PASSWORD')
 host = getenv('DB_HOST')
 database = getenv('DATABASE')
 
 
+app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{user}:{pwd}@{host}/{database}"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
-class User(db.Model):
+tags = db.Table('domains',
+                db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+                db.Column('domains', db.Integer, db.ForeignKey('domain.id'), primary_key=True)
+                )
+
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String)
+    level = db.Column(db.Integer)
+    domains = db.relationship('Domain', secondary=tags, lazy='subquery',
+                              backref=db.backref('users', lazy=True))
+
+
+class Domain(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(100))
 
 
 @app.route('/')
